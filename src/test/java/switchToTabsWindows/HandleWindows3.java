688fdb9ae50aed0483ab9bfe6468f3c20af13142
Avishek Bhattacharjee <avishek350751@gmail.com>
2025-05-15 16:09:05 -0500

    switch to tabs

--- src/test/java/switchToTabsWindows/HandleWindows3.java
+++ src/test/java/switchToTabsWindows/HandleWindows3.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HandleWindows2 {
+public class HandleWindows3 {
 
 	public static void main(String[] args) throws InterruptedException {
 		
@@ -19,32 +19,12 @@ public class HandleWindows2 {
 		String parent_window = driver.getWindowHandle();
 		System.out.println("Parent window is: " + parent_window);
 		
-		List<WebElement> icons = driver.findElements(By.xpath("//div[@id='login_container']//div[@class='social-btns']/a"));
-		for(WebElement ele : icons) {
-			
-			ele.click();
-		}
 		
 
 		Set<String> all_windows = driver.getWindowHandles();
 		System.out.println("Total number of windows: " + all_windows);
 		
 		
-		for(String handle: all_windows) {
-			
-			driver.switchTo().window(handle);
-			String title = driver.getTitle();
-			System.out.println("Current title is: "+title);
-			if(title.contains("Facebook")) {
-				break;
-			}
-		}
-		
-		driver.findElement(By.name("email")).sendKeys("admin@gmail.com");
-		driver.findElement(By.name("pass")).sendKeys("password");
-		driver.close();
-		driver.switchTo().window(parent_window);
-		driver.quit();
 	}
 
 }
