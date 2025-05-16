621911bfd483cf3d88957979b65634e0cb55de23
avi350751 <avishek350751@gmail.com>
2025-05-15 19:15:50 -0500

    Iterator logic

--- src/test/java/switchToTabsWindows/HandleWindows3.java
+++ src/test/java/switchToTabsWindows/HandleWindows3.java
@@ -1,12 +1,13 @@
 package switchToTabsWindows;
 
-import java.util.List;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+
 
 public class HandleWindows3 {
 
@@ -19,12 +20,26 @@ public class HandleWindows3 {
 		String parent_window = driver.getWindowHandle();
 		System.out.println("Parent window is: " + parent_window);
 		
-		
+		driver.findElement(By.xpath("//div[@class='social']//a[contains(@href,'linkedin')]")).click();
 
 		Set<String> all_windows = driver.getWindowHandles();
 		System.out.println("Total number of windows: " + all_windows);
+		Iterator<String> itr = all_windows.iterator();
+		
+		while(itr.hasNext()) {
+			String child_window = itr.next();
+			if(!parent_window.equalsIgnoreCase(child_window)) {
+				driver.switchTo().window(child_window);
+				break;
+			}
+		}
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@data-modal='base-sign-in-modal']")).click();
 		
 		
-	}
+		driver.close();
+		driver.switchTo().window(parent_window);
+		driver.quit();
 
+}
 }
